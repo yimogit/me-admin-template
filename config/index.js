190fad1e2589400270768b336ba769d30f386bc0
yimo <1280160831@qq.com>
2018-05-23 00:16:40 +0800

    fix

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    // assetsPublicPath: './',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
+    // assetsPublicPath: '/',
 
     /**
      * Source Maps
